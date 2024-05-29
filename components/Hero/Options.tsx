9527da2b4c1e40bf55f19cde135b03e356812ca5
atulRaaj <atulraj1010@tutanota.com>
2024-05-29 14:09:40 +0530

    Working panes

--- components/Hero/Options.tsx
+++ components/Hero/Options.tsx
@@ -2,6 +2,24 @@
 import React, { useState } from "react";
 import "./Options.scss";
 
+const optionsData = [
+  {
+    url: "https://66.media.tumblr.com/6fb397d822f4f9f4596dff2085b18f2e/tumblr_nzsvb4p6xS1qho82wo1_1280.jpg",
+    title: "Virtual",
+    description: "Description for option 1",
+  },
+  {
+    url: "https://66.media.tumblr.com/8b69cdde47aa952e4176b4200052abf4/tumblr_o51p7mFFF21qho82wo1_1280.jpg",
+    title: "Chief Technical",
+    description: "Description for option 2",
+  },
+  {
+    url: "https://66.media.tumblr.com/5af3f8303456e376ceda1517553ba786/tumblr_o4986gakjh1qho82wo1_1280.jpg",
+    title: "Officer",
+    description: "Description for option 3",
+  },
+];
+
 const Options = () => {
   const [activeOption, setActiveOption] = useState(0);
 
@@ -11,23 +29,17 @@ const Options = () => {
 
   return (
     <div className="options">
-      {[
-        "https://66.media.tumblr.com/6fb397d822f4f9f4596dff2085b18f2e/tumblr_nzsvb4p6xS1qho82wo1_1280.jpg",
-        "https://66.media.tumblr.com/8b69cdde47aa952e4176b4200052abf4/tumblr_o51p7mFFF21qho82wo1_1280.jpg",
-        "https://66.media.tumblr.com/5af3f8303456e376ceda1517553ba786/tumblr_o4986gakjh1qho82wo1_1280.jpg",
-      ].map((url, index) => (
+      {optionsData.map((option, index) => (
         <div
           key={index}
-          className={`option ${index === activeOption ? "active" : ""}`}
+          className={`option ${index === activeOption ? "active" : ""} `}
           onClick={() => handleClick(index)}
-          style={{ backgroundImage: `url(${url})` }}
+          style={{ backgroundImage: `url(${option.url})` }}
         >
-          <div className="shadow" />
-          <div className="label">
-            <div className="icon"></div>
-            <div className="info">
-              <div className="main">Option {index + 1}</div>
-              <div className="sub">Description for option {index + 1}</div>
+          <div className="label absolute bottom-5 right-5 flex items-center">
+            <div className="info ml-3 text-white">
+              <div className="main text-lg font-semibold">{option.title}</div>
+              <div className="sub text-sm">{option.description}</div>
             </div>
           </div>
         </div>
--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 import { useState } from "react";
+import Options from "./Options";
 
 const Hero = () => {
   const [email, setEmail] = useState("");
@@ -11,17 +12,17 @@ const Hero = () => {
 
   return (
     <>
-      <section className="overflow-hidden pb-20 pt-35 md:pt-40 xl:pb-25 xl:pt-46">
+      <section className="-mt-15 overflow-hidden pb-20 pt-35 md:pt-40 xl:pb-25 xl:pt-46">
         <div className="mx-auto max-w-c-1390 px-4 md:px-8 2xl:px-0">
-          <div className="flex lg:items-center lg:gap-8 xl:gap-32.5">
-            <div className=" md:w-1/2">
+          <div className="flex flex-col flex-wrap lg:flex-row lg:flex-nowrap lg:items-center lg:justify-between lg:gap-8 xl:gap-32.5">
+            <div className="md:w-1/2">
               <h4 className="mb-4.5 text-lg font-medium text-black dark:text-white">
-                {/* Hire Virtual CTO Today{" "} */}
+                {/* Hire Virtual CTO Today */}
               </h4>
-              <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero ">
+              <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero">
                 Hire Virtual CTO
-                <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark ">
-                  Today{" "}
+                <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark">
+                  Today
                 </span>
               </h1>
               <p>
@@ -42,74 +43,12 @@ const Hero = () => {
                     className="flex rounded-full bg-gray-200 px-7.5 py-2.5 text-black duration-300 ease-in-out hover:bg-blackho dark:bg-btndark dark:hover:bg-blackho"
                   >
                     Learn More
-                  </button>{" "}
+                  </button>
                 </div>
               </div>
-
-             
-              {/* <div className="mt-10">
-                <form onSubmit={handleSubmit}>
-                  <div className="flex flex-wrap gap-5">
-                    <input
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      type="text"
-                      placeholder="Enter your email address"
-                      className="rounded-full border border-stroke px-6 py-2.5 shadow-solid-2 focus:border-primary focus:outline-none dark:border-strokedark dark:bg-black dark:shadow-none dark:focus:border-primary"
-                    />
-                    <button
-                      aria-label="get started button"
-                      className="flex rounded-full bg-black px-7.5 py-2.5 text-white duration-300 ease-in-out hover:bg-blackho dark:bg-btndark dark:hover:bg-blackho"
-                    >
-                      Get Started
-                    </button>
-                  </div>
-                </form>
-
-                <p className="mt-5 text-black dark:text-white">
-                  Try for free no credit card required.
-                </p>
-              </div> */}
             </div>
-
-            <div className="animate_right hidden md:w-1/2 lg:block">
-              <div className="relative 2xl:-mr-7.5">
-                <Image
-                  src="/images/shape/shape-01.png"
-                  alt="shape"
-                  width={46}
-                  height={246}
-                  className="absolute -left-11.5 top-0"
-                />
-                <Image
-                  src="/images/shape/shape-02.svg"
-                  alt="shape"
-                  width={36.9}
-                  height={36.7}
-                  className="absolute bottom-0 right-0 z-10"
-                />
-                <Image
-                  src="/images/shape/shape-03.svg"
-                  alt="shape"
-                  width={21.64}
-                  height={21.66}
-                  className="absolute -right-6.5 bottom-0 z-1"
-                />
-                <div className=" relative aspect-[700/444] w-full">
-                  <Image
-                    className="shadow-solid-l dark:hidden"
-                    src="/images/hero/hero-light.svg"
-                    alt="Hero"
-                    fill
-                  />
-                  <Image
-                    className="hidden shadow-solid-l dark:block"
-                    src="/images/hero/hero-dark.svg"
-                    alt="Hero"
-                    fill
-                  />
-                </div>
-              </div>
+            <div className="lg:w-1/2">
+              <Options />
             </div>
           </div>
         </div>
